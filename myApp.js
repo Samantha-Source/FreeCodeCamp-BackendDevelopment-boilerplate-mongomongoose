fec866c41278a80afc7bdabac01fdd0fc414e189
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-10-04 20:10:21 -0400

    Project Setup

--- myApp.js
+++ myApp.js
@@ -1,4 +1,8 @@
+const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoURI = process.env.MONGO_URI;
+// console.log(process.env.MONGO_URI);
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 let Person;
