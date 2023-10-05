7ab0abbb076c31a12a6a4db7913654702216e81f
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-10-04 20:58:31 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -43,7 +43,12 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({
+    name: personName
+  }, (err, personFound) => {
+    if (err) return console.error(err);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
